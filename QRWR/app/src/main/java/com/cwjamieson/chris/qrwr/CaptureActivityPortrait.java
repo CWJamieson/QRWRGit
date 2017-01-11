c839744b61e90c053f8bcd05ffdd4cefc28ed51d
Christopher <christopherjohnjamieson@hotmail.com>
2017-01-11 17:36:04 -0500

    uploaded all

---
+++ QRWR/app/src/main/java/com/cwjamieson/chris/qrwr/CaptureActivityPortrait.java
@@ -0,0 +1,10 @@
+package com.cwjamieson.chris.qrwr;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by Chris on 12/29/2016.
+ */
+
+public class CaptureActivityPortrait extends CaptureActivity {
+}
---
+++ QRWR/app/src/main/java/com/cwjamieson/chris/qrwr/ReadScreen.java
@@ -0,0 +1,75 @@
+package com.cwjamieson.chris.qrwr;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ReadScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_screen);
+
+        //set back button and title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Scan a QR code");
+
+        //start camera read
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureActivityPortrait.class);
+        integrator.setOrientationLocked(true);
+        integrator.initiateScan();
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id==android.R.id.home) {
+            finish();
+        }
+        return true;
+
+    }
+
+
+    //upon reading a code
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                finish();
+            } else {
+                alert(result.getContents());
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    private void alert(String s)
+    {
+
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage(s);
+        dlgAlert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //write to never show again
+                    }
+                });
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
+
+}
